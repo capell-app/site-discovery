c378998c2c441f8213bb60918284bf3b1e5425a3
Ben <howdu@users.noreply.github.com>
2026-07-13 18:23:24 +0100

    fix: align extension manifests with v1 API

--- src/Manifest/SiteDiscoveryFrontendRoutesContribution.php
+++ src/Manifest/SiteDiscoveryFrontendRoutesContribution.php
@@ -11,6 +11,6 @@ final class SiteDiscoveryFrontendRoutesContribution implements ExtensionContribu
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^4.0';
+        return '^1.0';
     }
 }
